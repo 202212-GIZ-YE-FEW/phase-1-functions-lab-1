4f4464be19ac277889d0f34ae5365a634f5e0e27
Mohammed Al-jaberi <aljabri606@gmail.com>
2023-02-21 16:45:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockCustomer){
+  if (blockCustomer>=42){
+   return 42 - blockCustomer ;
+  
+  }
+  else
+  return blockCustomer - 42 ; 
+}
+
+function distanceFromHqInFeet(blockCustomer){
+const distance =  distanceFromHqInFeet(blockCustomer)
+return distance*264;
+}
+
+function distanceTravelledInFeet(start,end){
+  if (start>=end)
+  return (start-end)*264;
+  else
+  return (end-start)*264
+}
+
+function calculatesFarePrice(start, end) {
+ if (start>end){
+  const far = start - end
+  if(far>=400 && far<=2000)
+ }
+}
